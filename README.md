a14b096c8ebf6e6e36c47e8a3d49930de2bcfcf6
canovasjm <juanmacanovas@gmail.com>
2022-08-31 22:54:41 -0300

    update README and query file

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # sql-times  
 
 WIP for a blog post about aggregating time series (aka *downsampling*) with SQL
+
+1. Clone the repo: `git clone https://github.com/canovasjm/sql-times.git`  
+
+2. Open a terminal and `cd` into the cloned repo  
+
+3. Type `docker-compose up -d`  
+
+4. Go to the browser and in the navigation bar type `http://localhost:8080`  
+
+5. Log into pgAdmin with the following credentials:  
+    user: `admin@admin.com`  
+    password: `admin`  
+
+6. Create a server
+
+7. Log into Postgres SQL with the following credentials:  
+    POSTGRES_USER: `root`  
+    POSTGRES_PASSWORD: `root`  
+
+8. Once you have finished, go back to the terminal and type `docker-compose down`     
+
+A word of caution: in steps 
--- query.sql
+++ query.sql
@@ -1,37 +1,38 @@
 -- CASE 1: seconds
--- drop the table
-DROP TABLE atable;
+
+-- drop the table if exits
+DROP TABLE IF EXISTS atable;
 
 -- create the table
 CREATE TABLE atable (
-  id text,
+  sensor_id text,
   time timestamp without time zone,
   value smallint
   )
 ;
 
 -- insert some sample data
-INSERT INTO atable (id, time, value) VALUES ('ABC', '2022-04-08 09:00:00', 27);
-INSERT INTO atable (id, time, value) VALUES ('ABC', '2022-04-08 09:00:01', 30);
-INSERT INTO atable (id, time, value) VALUES ('ABC', '2022-04-08 09:00:21', 22);
+INSERT INTO atable (sensor_id, time, value) VALUES ('ABC', '2022-04-08 09:00:00', 27);
+INSERT INTO atable (sensor_id, time, value) VALUES ('ABC', '2022-04-08 09:00:01', 30);
+INSERT INTO atable (sensor_id, time, value) VALUES ('ABC', '2022-04-08 09:00:21', 22);
 
-INSERT INTO atable (id, time, value) VALUES ('ABC', '2022-04-08 09:01:14', 31);
-INSERT INTO atable (id, time, value) VALUES ('ABC', '2022-04-08 09:01:21', 28);
-INSERT INTO atable (id, time, value) VALUES ('ABC', '2022-04-08 09:01:38', 35);
+INSERT INTO atable (sensor_id, time, value) VALUES ('ABC', '2022-04-08 09:01:14', 31);
+INSERT INTO atable (sensor_id, time, value) VALUES ('ABC', '2022-04-08 09:01:21', 28);
+INSERT INTO atable (sensor_id, time, value) VALUES ('ABC', '2022-04-08 09:01:38', 35);
 
-INSERT INTO atable (id, time, value) VALUES ('ABC', '2022-04-08 09:02:07', 33);
-INSERT INTO atable (id, time, value) VALUES ('ABC', '2022-04-08 09:02:11', 19);
-INSERT INTO atable (id, time, value) VALUES ('ABC', '2022-04-08 09:02:56', 25);
+INSERT INTO atable (sensor_id, time, value) VALUES ('ABC', '2022-04-08 09:02:07', 33);
+INSERT INTO atable (sensor_id, time, value) VALUES ('ABC', '2022-04-08 09:02:11', 19);
+INSERT INTO atable (sensor_id, time, value) VALUES ('ABC', '2022-04-08 09:02:56', 25);
 
-INSERT INTO atable (id, time, value) VALUES ('XYZ', '2022-04-08 09:00:00', 26);
-INSERT INTO atable (id, time, value) VALUES ('XYZ', '2022-04-08 09:00:25', 37);
-INSERT INTO atable (id, time, value) VALUES ('XYZ', '2022-04-08 09:00:47', 29);
+INSERT INTO atable (sensor_id, time, value) VALUES ('XYZ', '2022-04-08 09:00:00', 26);
+INSERT INTO atable (sensor_id, time, value) VALUES ('XYZ', '2022-04-08 09:00:25', 37);
+INSERT INTO atable (sensor_id, time, value) VALUES ('XYZ', '2022-04-08 09:00:47', 29);
 
--- see what we have in atable
+-- see what we have in `atable`
 SELECT * FROM atable;
 
 -- see how date_trunc() works
-SELECT id, date_trunc('minute', time) AS truncated_time, value FROM atable;
+SELECT sensor_id, date_trunc('minute', time) AS truncated_time, value FROM atable;
 
 -- query to get only the most recent data point within a minute
 SELECT
@@ -39,68 +40,70 @@ SELECT
 FROM atable t
   INNER JOIN (
     SELECT
-      id,
+      sensor_id,
       MAX(time) AS time
     FROM atable
-    --GROUP BY id, DIV(extract(epoch FROM time)::integer, 60)
-    GROUP BY id, date_trunc('minute', time)
-  ) m ON t.time = m.time AND t.id = m.id
-ORDER BY id, time;
+    --GROUP BY sensor_id, DIV(extract(epoch FROM time)::integer, 60)
+    GROUP BY sensor_id, date_trunc('minute', time)
+  ) m ON t.time = m.time AND t.sensor_id = m.sensor_id
+ORDER BY sensor_id, time;
 
 -- query to get only the most recent data point within a minute, with CTE
 WITH times_cte AS
 (
 	SELECT
-		id,
+		sensor_id,
 		MAX(time) AS time
 	FROM atable
-  --GROUP BY id, DIV(extract(epoch FROM time)::integer, 60)
-	GROUP BY id, date_trunc('minute', time)
+  --GROUP BY sensor_id, DIV(extract(epoch FROM time)::integer, 60)
+	GROUP BY sensor_id, date_trunc('minute', time)
 )
 SELECT
   t.*  /* you might want to be more specific here */
 FROM atable t
   INNER JOIN times_cte m 
-  	ON t.time = m.time AND t.id = m.id
-ORDER BY id, time;
+  	ON t.time = m.time AND t.sensor_id = m.sensor_id
+ORDER BY sensor_id, time;
 
 
-
-
+--------------------------------------------------------------------------
 -- CASE 2: miliseconds
--- drop the table
-DROP TABLE btable;
+-- This is another example, this time timestamps have up to the milisecond
+-- part and we want to downsample to the second.  
+
+-- drop the table if exits
+DROP TABLE IF EXISTS btable;
 
 -- create the table
 CREATE TABLE btable (
-  id text,
+  sensor_id text,
   time timestamp without time zone,
   value smallint
   )
 ;
 
 -- insert some sample data
-INSERT INTO btable (id, time, value) VALUES ('DEF', '2022-04-12 16:00:00.113', 98);
-INSERT INTO btable (id, time, value) VALUES ('DEF', '2022-04-12 16:00:00.231', 75);
-INSERT INTO btable (id, time, value) VALUES ('DEF', '2022-04-12 16:00:00.578', 81);
+INSERT INTO btable (sensor_id, time, value) VALUES ('DEF', '2022-04-12 16:00:00.113', 98);
+INSERT INTO btable (sensor_id, time, value) VALUES ('DEF', '2022-04-12 16:00:00.231', 75);
+INSERT INTO btable (sensor_id, time, value) VALUES ('DEF', '2022-04-12 16:00:00.578', 81);
 
-INSERT INTO btable (id, time, value) VALUES ('DEF', '2022-04-12 16:00:01.188', 55);
-INSERT INTO btable (id, time, value) VALUES ('DEF', '2022-04-12 16:00:01.325', 70);
-INSERT INTO btable (id, time, value) VALUES ('DEF', '2022-04-12 16:00:01.762', 83);
+INSERT INTO btable (sensor_id, time, value) VALUES ('DEF', '2022-04-12 16:00:01.188', 55);
+INSERT INTO btable (sensor_id, time, value) VALUES ('DEF', '2022-04-12 16:00:01.325', 70);
+INSERT INTO btable (sensor_id, time, value) VALUES ('DEF', '2022-04-12 16:00:01.762', 83);
 
-INSERT INTO btable (id, time, value) VALUES ('DEF', '2022-04-12 16:00:02.114', 76);
-INSERT INTO btable (id, time, value) VALUES ('DEF', '2022-04-12 16:00:02.349', 71);
-INSERT INTO btable (id, time, value) VALUES ('DEF', '2022-04-12 16:00:02.629', 87);
+INSERT INTO btable (sensor_id, time, value) VALUES ('DEF', '2022-04-12 16:00:02.114', 76);
+INSERT INTO btable (sensor_id, time, value) VALUES ('DEF', '2022-04-12 16:00:02.349', 71);
+INSERT INTO btable (sensor_id, time, value) VALUES ('DEF', '2022-04-12 16:00:02.629', 87);
 
-INSERT INTO btable (id, time, value) VALUES ('RST', '2022-04-12 16:00:00.147', 13);
-INSERT INTO btable (id, time, value) VALUES ('RST', '2022-04-12 16:00:00.451', 24);
-INSERT INTO btable (id, time, value) VALUES ('RST', '2022-04-12 16:00:00.924', 31);
+INSERT INTO btable (sensor_id, time, value) VALUES ('RST', '2022-04-12 16:00:00.147', 13);
+INSERT INTO btable (sensor_id, time, value) VALUES ('RST', '2022-04-12 16:00:00.451', 24);
+INSERT INTO btable (sensor_id, time, value) VALUES ('RST', '2022-04-12 16:00:00.924', 31);
 
--- see what we have in btable
+-- see what we have in `btable`
 SELECT * FROM btable;
 
 -- see how date_trunc() works
-SELECT id, date_trunc('second', time) AS truncated_time, value FROM btable;
+SELECT sensor_id, date_trunc('second', time) AS truncated_time, value FROM btable;
 
 -- query to get only the most recent data point within a second
 SELECT
@@ -108,10 +111,10 @@ SELECT
 FROM btable t
   INNER JOIN (
     SELECT
-      id,
+      sensor_id,
       MAX(time) AS time
     FROM btable
-    --GROUP BY id, DIV(extract(epoch FROM time)::integer, 1000)
-    GROUP BY id, date_trunc('second', time)
-  ) m ON t.time = m.time AND t.id = m.id
-ORDER BY id, time;
+    --GROUP BY sensor_id, DIV(extract(epoch FROM time)::integer, 1000)
+    GROUP BY sensor_id, date_trunc('second', time)
+  ) m ON t.time = m.time AND t.sensor_id = m.sensor_id
+ORDER BY sensor_id, time;
